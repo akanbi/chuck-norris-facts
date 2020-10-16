e35b3e1d1dc4aedacf46643482e7b97bf1e85cbb
Akanbi Strossi <akanbi87@gmail.com>
2020-10-16 16:44:16 -0300

    Renamed activity and organized file structure

--- app/src/main/java/com/akanbi/chucknorris/presenter/view/ChuckNorrisFactsActivity.kt
+++ app/src/main/java/com/akanbi/chucknorris/presenter/view/ChuckNorrisFactsActivity.kt
@@ -1,12 +1,12 @@
 package com.akanbi.chucknorris.presenter.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.akanbi.chucknorris.R
 
-class MainActivity : AppCompatActivity() {
+class ChuckNorrisFactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chuck_norris_facts)
     }
 }
