058c608298e641e573b4593cd8d520cd85371efb
Akanbi Strossi <akanbi87@gmail.com>
2020-10-16 17:24:54 -0300

    Adjusted layout with styles and toolbar

--- app/src/main/java/com/akanbi/chucknorris/presenter/view/ChuckNorrisFactsActivity.kt
+++ app/src/main/java/com/akanbi/chucknorris/presenter/view/ChuckNorrisFactsActivity.kt
@@ -3,10 +3,14 @@ package com.akanbi.chucknorris.presenter.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.akanbi.chucknorris.R
+import kotlinx.android.synthetic.main.activity_chuck_norris_facts.*
 
 class ChuckNorrisFactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chuck_norris_facts)
+        
+        toolbarActivity.title = resources.getString(R.string.app_name)
+        setSupportActionBar(toolbarActivity)
     }
 }
